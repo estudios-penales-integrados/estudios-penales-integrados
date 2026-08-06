552da629ce351c39400a76d101a5957666755235
estudios-penales-integrados <estudiospenalesintegrados@gmail.com>
2026-08-06 13:16:53 -0300

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,12 +1,42 @@
 export const SITE = {
   name: "Estudios Penales Integrados",
 
-  email: "estudiospenalesintegrados@gmail.com",
+  url: "https://estudiospenalesintegrados.netlify.app",
+
+  description:
+    "Estudio jurídico especializado en Derecho Penal. Defensa Penal, asesoramiento jurídico, ejecución penal, asistencia a víctimas, delitos económicos y compliance.",
+
+  keywords: [
+    "abogado penal",
+    "abogado penalista",
+    "derecho penal",
+    "defensa penal",
+    "estudio jurídico",
+    "abogado Río Negro",
+    "abogado Viedma",
+    "abogado Buenos Aires",
+    "delitos económicos",
+    "compliance penal",
+    "ejecución penal",
+    "defensa criminal",
+    "asesoramiento penal",
+    "abogado federal"
+  ],
 
   whatsapp: "5492920271591",
 
   whatsappMessage:
-    "Hola. Me gustaría realizar una consulta jurídica.",
+    "Hola, quisiera realizar una consulta.",
 
-  city: "Viedma, Río Negro"
+  email:
+    "estudiospenalesintegrados@gmail.com",
+
+  city:
+    "Viedma, Río Negro",
+
+  province:
+    "Río Negro",
+
+  country:
+    "Argentina"
 };

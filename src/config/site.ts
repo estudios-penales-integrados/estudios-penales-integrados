abc7e503cedc091fe6f749cb33d26289c3405757
estudios-penales-integrados <estudiospenalesintegrados@gmail.com>
2026-08-06 09:58:18 -0300

    Create site.ts

---
+++ src/config/site.ts
@@ -0,0 +1,22 @@
+export const SITE = {
+
+  name: "Estudios Penales Integrados",
+
+  email: "estudiospenalesintegrados@gmail.com",
+
+  whatsapp: "5492920271591",
+
+  whatsappMessage:
+    "Hola. Quisiera realizar una consulta jurídica.",
+
+  city: "Viedma, Río Negro",
+
+  jurisdictions: [
+    "Justicia Federal",
+    "Provincia de Río Negro",
+    "Provincia de Buenos Aires"
+  ],
+
+  schedule: "Lunes a Viernes · 9:00 a 20:00 hs - URGENCIAS 24 HS"
+
+};
